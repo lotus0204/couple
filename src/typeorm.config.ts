c629ed31722b726a0c931ddc49a70a8bb46225fb
Park Sang Yeop <81801012+lotus0204@users.noreply.github.com>
2023-06-04 20:36:27 +0900

    ✨DB 설정

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -2,15 +2,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: '172.18.0.1',
+  host: 'localhost',
   port: 3306,
-  username: 'root',
-  password: 'dpstldzm',
+  username: 'dev',
+  password: 'mildangdev1357',
   database: 'couple',
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  entities: ['dist/**/*.entity.js'],
   synchronize: true,
-  migrations: ["dist/migrations/*{.ts,.js}"],
-  migrationsTableName: "migrations", // migration 내용이 기록될 테이블명(default = migration)
+  // migrations: ["dist/migrations/*{.ts,.js}"],
+  // migrationsTableName: "migrations", // migration 내용이 기록될 테이블명(default = migration)
 };
 
 export default config;
